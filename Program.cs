ca76c56304c695b159586bf55890ed16eb875a0a
Jessica <jgonzales0688@gmail.com>
2023-02-04 20:44:27 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SecondMethodsExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine("Type in an animal");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Type in a band name");
+            var band = Console.ReadLine();
+
+            Console.WriteLine($"Funny sentence: {name} had a band named {band} " +
+                $"and its mascot was a {color} {animal}");
+        }
+    }
+}
